c2811235b9ec47e72e1bb1b112848d50decaf61a
Kavishka Jayasinghe <kavishkajaye@gmail.com>
2026-04-28 15:15:33 +0530

    Fixed gateway path rewriting for auth service

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -13,7 +13,7 @@ proxy.on('error', (err, req, res) => {
 // Route requests to the Auth Service
 app.use("/auth", (req, res) => {
     // 'auth-service' must match the name you gave the Container App in Azure change for triggering locally
-    req.url = req.url.replace(/^\//, '');
+    req.url = req.url.replace(/^\/auth/, '');
     proxy.web(req, res, { target: "http://auth-service" });
 });
 
